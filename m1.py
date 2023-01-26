9d61e9b85140a81842535b0197f3e97ab560752e
amirghari <ghariamir97@gmail.com>
2023-01-26 14:30:45 +0000

    Graph-Added

---
+++ m1.py
@@ -0,0 +1,20 @@
+import numpy as np
+import math
+from matplotlib import pyplot as plt
+
+
+ang = np.arange(180)
+xo = np.sin(np.radians(ang))
+noise = ((np.random.rand(180)*2)-1.0)/100.0
+xn = xo+noise
+plt.subplot(121)
+plt.ylabel('sine')
+plt.xlabel('degrees')
+plt.plot(xo)
+plt.subplot(122)
+plt.ylabel('sine')
+plt.xlabel('degrees')
+plt.plot(xn)
+plt.suptitle('code20-2')
+plt.show()
+plt.savefig('m1.pdf') 
